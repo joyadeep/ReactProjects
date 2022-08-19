4ee331824a15fbe555ce007082000578b7c51b2e
joyadeep <joyadeep.limbu@varosatech.com>
2022-08-19 23:01:06 +0545

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Typography } from '@mui/material'
+
+function App() {
+ 
+  return (
+  <Typography variant='h5'>Stat your project Here</Typography>
+  
+  )
+}
+
+export default App
